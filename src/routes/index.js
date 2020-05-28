bc613498cf70e64e673115b767c9811977d2dc55
gustavo <gustavo.oliveira@atuo.in>
2020-05-28 14:20:06 -0300

    realizado configurações iniciais: creado organização de diretório instalado react-navigation criado arquivo de home

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from '../pages/home';
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes
