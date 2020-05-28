3209174d5bfffa09e4a325be275202c7bf337b1d
gustavo <gustavo.oliveira@atuo.in>
2020-05-28 20:00:06 -0300

    corrigido quebra da aplicação ao traze os dados da api

--- src/pages/pokemons/index.js
+++ src/pages/pokemons/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 
+import axios from 'axios';
+
 const Pokemons = () => {
+    const [ data, setData ] = useState([]);
+
+    const getPokemons = () => {
+         fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+            .then(res => res.json())
+            .then( async pokemons =>
+                await setData(pokemons.results)
+            )
+    }
+
+    useEffect(() => {
+        getPokemons();
+    }, [])
+
     return (
         <View>
-            <Text>
-                Pokemons
-            </Text>
+            {
+                data.map(pokemon => (
+                    <Text key={pokemon.name}>
+                        {pokemon.name}
+                    </Text>
+                    
+                ))
+            }
         </View>
     )
 }
