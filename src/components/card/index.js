a6276dd8622aeed721f05fd0edd591a945467ae7
gustavo <gustavo.oliveira@atuo.in>
2020-05-28 21:55:15 -0300

    ai meu deus não funcona

---
+++ src/components/card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+const PokemonCard = props => {
+    console.log('teste', props);
+    
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default PokemonCard;
--- src/pages/pokemons/index.js
+++ src/pages/pokemons/index.js
@@ -1,34 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
 
-import axios from 'axios';
+import PokemonCard from '../../components/card';
+import { View, Text } from 'react-native';
 
 const Pokemons = () => {
     const [ data, setData ] = useState([]);
+    const [ pokeData, setPokeData ] = useState([]);
+    const [ loading, setloading ] = useState(true);
 
-    const getPokemons = () => {
-         fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+    const fetchPokemons = () => {
+        fetch(`${initialUrl}`)
             .then(res => res.json())
-            .then( async pokemons =>
+            .then(async pokemons =>
                 await setData(pokemons.results)
             )
     }
 
+    const fetchPokemonInfo = () => {
+        data.map(pokemon => (
+            fetch(pokemon.url)
+                .then(res => res.json())
+                .then(async pokemonInfo =>            
+                    await setPokeData(pokemonInfo) 
+                )
+        ))
+    }
+
     useEffect(() => {
-        getPokemons();
+        fetchPokemons();
     }, [])
 
+    useEffect(() => {
+       fetchPokemonInfo();
+    }, [])
+    
+    console.log(pokeData);
+    
+    
     return (
-        <View>
+        <>
             {
-                data.map(pokemon => (
-                    <Text key={pokemon.name}>
-                        {pokemon.name}
-                    </Text>
-                    
-                ))
+                // pokeData.map(pokemon => (
+                //     <PokemonCard 
+                //         key={pokemon.order}
+                //         pokemon={pokemon}
+                //     />
+                // ))
             }
-        </View>
+        </>
     )
 }
 
